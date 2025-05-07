78dfc16dfbec076be91d91ab24278ca6f3a6d4bf
ABHISHEK <abhidk62@gmail.com>
2025-05-07 15:47:15 +0530

    Time: 41 ms (53.29%), Space: 55 MB (5.98%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World"
+        
+    }
+};
+var res=createHelloWorld()
+var ans=res()
+console.log(ans)
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
