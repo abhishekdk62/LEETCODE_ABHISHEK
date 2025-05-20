a57eaa18c36a929194ee78fc0c2d96515ad75d45
ABHISHEK <abhidk62@gmail.com>
2025-05-20 19:07:31 +0530

    Time: 15 ms (56.06%), Space: 57.7 MB (60.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length!=t.length) {return false}
+
+let map={}
+    s.split("").map((v)=>{
+
+if(map[v])
+{
+    map[v]=map[v]+1
+}else
+{
+    map[v]=1
+}
+    })
+    
+let map2={}
+t.split("").map((v)=>{
+    if(map2[v])
+{
+    map2[v]=map2[v]+1
+}else
+{
+    map2[v]=1
+}
+})
+console.log(map)
+console.log(map2)
+let flag=false
+for(let i in map)
+{
+    if(map[i]==map2[i])
+    {
+        flag=true
+    }
+    else
+    {
+        return false
+    }
+}
+
+
+
+return flag
+};
