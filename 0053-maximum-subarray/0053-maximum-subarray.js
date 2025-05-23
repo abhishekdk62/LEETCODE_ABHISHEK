f5c74fb29355a293b91945e4ab9733c7f216a764
ABHISHEK <abhidk62@gmail.com>
2025-05-23 16:20:07 +0530

    Time: 7 ms (8.14%), Space: 63.3 MB (79.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -4,9 +4,9 @@
  */
 var maxSubArray = function(nums) {
 
-let sum=nums[0]
-let largesum=nums[0]
-nums.slice(1).forEach((v)=>{
+let sum=-Infinity
+let largesum=-Infinity
+nums.forEach((v)=>{
     sum=sum+v
     if(sum<v)
     {
