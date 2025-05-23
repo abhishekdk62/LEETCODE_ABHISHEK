f9e8f68b402f200391f6b688daf1de1c41f09989
ABHISHEK <abhidk62@gmail.com>
2025-05-23 09:50:12 +0530

    Time: 0 ms (100%), Space: 57.2 MB (64.64%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+    let current=head
+    let prev=null
+  while(current)
+  {
+      let next=current.next
+    current.next=prev
+    prev=current
+    current=next
+  }
+  //in vs code you just make the prev as this.head
+return prev
+
+    
+};
