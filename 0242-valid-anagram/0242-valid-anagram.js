62293e6c5ccdade13debe9fc7cc3c9aceb415488
ABHISHEK <abhidk62@gmail.com>
2025-05-23 20:33:59 +0530

    Time: 12 ms (73.18%), Space: 59.7 MB (25.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,46 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  
-
-let map={}
-    s.split("").map((v)=>{
-
-if(map[v])
-{
-    map[v]=map[v]+1
-}else
-{
-    map[v]=1
-}
-    })
-    
-let map2={}
-t.split("").map((v)=>{
-    if(map2[v])
-{
-    map2[v]=map2[v]+1
-}else
-{
-    map2[v]=1
-}
-})
-console.log(map)
-console.log(map2)
-let flag=false
-for(let i in map)
-{
-    if(map[i]==map2[i])
+    if(s.length!=t.length)
     {
-        flag=true
+        return false
+    }
+let map={}
+s.split("").forEach((v)=>{
+map[v]=(map[v]||0)+1
+})
+for(let char of t)
+{
+    if(map[char])
+    {
+        map[char]=map[char]-1
     }
     else
     {
         return false
     }
 }
-
-
-
-return flag
+return true
+    
 };
