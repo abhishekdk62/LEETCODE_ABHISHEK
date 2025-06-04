0e484aadbd26b605fce783b329f831af687f412a
ABHISHEK <abhidk62@gmail.com>
2025-06-04 16:07:09 +0530

    Time: 2 ms (70.84%), Space: 59 MB (42.65%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -10,20 +10,25 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function (root) {
-    if (!root) return []
-    let quee = [root]
-    let res = []
-    while (quee.length > 0) {
-    let size = quee.length
-        let val = []
-        for (let i = 0; i < size; i++) {
-            let node = quee.shift()
-            if (node.left) quee.push(node.left)
-            if (node.right) quee.push(node.right)
-            val.push(node.val)
+var levelOrder = function(root) {
+    if(!root) return []
+    let node=root
+    let result=[]
+
+    let quee=[node]
+
+    while(quee.length)
+    {let vals=[]
+        let l=quee.length
+        for(let i=0;i<l;i++)
+        {
+
+        let node=quee.shift()
+        if(node.left) quee.push(node.left)
+        if(node.right) quee.push(node.right)
+        vals.push(node.val)
         }
-        res.push(val)
+        result.push(vals)
     }
-    return res
+    return(result)
 };
