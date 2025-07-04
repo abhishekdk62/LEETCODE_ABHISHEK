36d86986771f8f1044a0f44f593bb401c4b21651
ABHISHEK <abhidk62@gmail.com>
2025-07-05 00:22:09 +0530

    Time: 49 ms (74.09%), Space: 59.3 MB (38.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,20 +11,15 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-
-    let fast=head
     let slow=head
-while(fast &&fast.next)
-{
-        fast=fast.next.next
-    slow=slow.next
-    if(slow==fast)
-{
-    return true
-}
+    let fast=head
 
+while(fast&&fast.next)
+{
+    fast=fast.next.next
+    slow=slow.next
+    if(fast==slow) return true
 }
 return false
-
     
 };
