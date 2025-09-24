2c4d0dde959386cea84bf35d4fd3aa8a93bb7444
ABHISHEK <abhidk62@gmail.com>
2025-09-24 14:11:20 +0530

    Time: 0 ms (100%), Space: 55.6 MB (38.4%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.js
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.js
@@ -2,36 +2,30 @@
  * @param {string} num
  * @return {string}
  */
-var largestGoodInteger = function (num) {
-    let last = ""
-    let count = 0
-    let max = -Infinity
-    nums = num.split("")
-    nums.forEach((v) => {
-
-        if (v == last) {
+var largestGoodInteger = function(num) {
+    let past=""
+    let count=0
+    let max=-Infinity
+    num.split("").forEach((v)=>{
+        if(v==past)
+        {
             count++
+        }else
+        {
+            past=v
+            count=1
         }
-        else {
 
-            count = 1
-            last = v
-        }
-        if (count == 3) {
-            if (v > max) {
-                max = v
-
-            }
-
+        if(count==3&&v>max)
+        {
+            max=v
         }
 
     })
-    let ans = ""
-    console.log(max)
     if(max==-Infinity)
     {
         return ""
     }
-    return ans + max + max + max
-
+return max+max+max
+    
 };
