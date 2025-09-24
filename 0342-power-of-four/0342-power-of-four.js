0af058a2facd2cb819612302b3b8aa70e7fc3714
ABHISHEK <abhidk62@gmail.com>
2025-09-24 15:44:05 +0530

    Time: 0 ms (100%), Space: 55.8 MB (84.33%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -2,8 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-    if (n <= 0) return false;         
-    if ((n & (n - 1)) !== 0) return false; 
-    return (n - 1) % 3 === 0;         
+var isPowerOfFour = function (n) {
+    if (n <= 0) return false;
+    if ((n & (n - 1)) !== 0) {
+        return false;
+    }
+    return (n - 1) % 3 === 0
+
 };
