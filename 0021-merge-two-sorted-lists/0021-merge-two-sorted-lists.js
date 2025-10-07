0afdefae74be08456aaace5802d891c678d76bcb
ABHISHEK <abhidk62@gmail.com>
2025-10-07 11:56:32 +0530

    Time: 0 ms (100%), Space: 57.4 MB (54.7%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -12,6 +12,7 @@
  */
 var mergeTwoLists = function(list1, list2) {
     let dummy=new ListNode(-1)
+
     let cur=dummy
     while(list1!=null&&list2!=null)
     {
@@ -19,14 +20,23 @@ var mergeTwoLists = function(list1, list2) {
         {
             cur.next=list2
             list2=list2.next
+          
         }else
         {
             cur.next=list1
             list1=list1.next
         }
-        cur=cur.next
+            cur=cur.next
+
+        
     }
-    if(list1==null) cur.next=list2
-    if(list2==null) cur.next=list1
+            if(list1==null)
+            {
+                cur.next=list2
+            }
+            if(list2==null){
+                cur.next=list1
+            }
     return dummy.next
+    
 };
