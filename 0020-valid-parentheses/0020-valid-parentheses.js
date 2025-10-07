f09fbc754f892cef6b3508a34cc65dc772caa6ef
ABHISHEK <abhidk62@gmail.com>
2025-10-07 12:07:11 +0530

    Time: 3 ms (65.92%), Space: 55.2 MB (82.06%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,28 +4,28 @@
  */
 var isValid = function (s) {
 
-    let pairs = { ")": "(", "}": "{", "]": "[" }
-let flag=true
     let stack = []
-
-    s.split("").forEach((v) => {
-        if (v == "(" || v == "{" || v == "[") {
-            stack.push(v)
-
+    let map = { ')': '(', '}': '{', ']': '[' }
+    let a = s.split("")
+    let flag = true
+    for (let k of a) {
+        if (k == '(' || k == '[' || k == '{') {
+            stack.push(k)
         } else {
-            let popped = stack.pop()
-       
-            if (pairs[v] != popped) {
-                console.log('nope')
-                flag= false
+            let ans = stack.pop()
+            if (map[k] != ans) {
+                return false
+                
             }
-
         }
-    })
-    if(stack.length!=0) flag=false
-    if(!flag) return false
-    return true
-
+    }
+    if (stack.length != 0) return false
+    if (flag) {
+        return true
+    }
+    else {
+        return false
+    }
 
 
 };
