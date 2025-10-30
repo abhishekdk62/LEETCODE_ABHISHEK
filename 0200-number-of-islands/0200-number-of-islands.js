bee8435cc26b8b041d633783fc74f79db9994653
ABHISHEK <abhidk62@gmail.com>
2025-10-30 12:41:23 +0530

    Time: 59 ms (79.13%), Space: 58.3 MB (73.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,27 +2,28 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
+var numIslands = function (grid) {
 let count=0
-    for(let i=0;i<grid.length;i++)
-    {
-        for(let j=0;j<grid[0].length;j++)
-        {
-if(grid[i][j]=='1')
-{
-    count++
-    check(i,j)
-}
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+
+            if(grid[i][j]=='1')
+            {
+                count++
+                check(i,j)
+
+            }
+
         }
     }
     function check(i,j)
     {
-        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0') return 0
-        grid[i][j]='0'
-        check(i+1,j)
-        check(i,j+1)
-        check(i,j-1)
-        check(i-1,j)
+if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0') return
+grid[i][j]='0'
+check(i,j-1)
+check(i+1,j)
+check(i,j+1)
+check(i-1,j)
     }
-    return count
+return count
 };
