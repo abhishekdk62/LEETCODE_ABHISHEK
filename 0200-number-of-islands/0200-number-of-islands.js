cf6439709918fba003a83efabb69ad204c93bd46
ABHISHEK <abhidk62@gmail.com>
2025-10-30 11:49:26 +0530

    Time: 57 ms (84.5%), Space: 59 MB (55.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -2,29 +2,27 @@
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function (grid, map = new Map()) {
-
-    let count = 0
-    function c(grid) {
-        for (let i = 0; i < grid.length; i++) {
-            for (let j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == "1") {
-                    console.log(count)
-                    count++
-                    check(i, j)
-
-                }
-            }
+var numIslands = function(grid) {
+let count=0
+    for(let i=0;i<grid.length;i++)
+    {
+        for(let j=0;j<grid[0].length;j++)
+        {
+if(grid[i][j]=='1')
+{
+    count++
+    check(i,j)
+}
         }
     }
-    function check(i, j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == "0") return 0
-        grid[i][j] = "0"
-        check(i + 1, j)
-        check(i, j + 1)
-        check(i - 1, j)
-        check(i, j - 1)
+    function check(i,j)
+    {
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0') return 0
+        grid[i][j]='0'
+        check(i+1,j)
+        check(i,j+1)
+        check(i,j-1)
+        check(i-1,j)
     }
-    c(grid)
     return count
 };
