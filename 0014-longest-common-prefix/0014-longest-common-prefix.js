c21e4c0eb400d037314128a2595559d62639cb7d
ABHISHEK <abhidk62@gmail.com>
2025-11-07 10:59:42 +0530

    Time: 0 ms (100%), Space: 54.3 MB (79.36%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+if(strs.length==0) {
+    return ""
+    }
+
+let prefix=''
+for(let i=0;i<strs[0].length;i++)
+
+{
+    let ch=strs[0][i]
+    for(let j=1;j<strs.length;j++)
+    {
+        if(ch!=strs[j][i])
+        {
+            return prefix 
+        }
+    }
+    prefix+=ch
+}
+    return prefix
+    
+};
