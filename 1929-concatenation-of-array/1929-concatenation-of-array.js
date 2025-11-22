03d8e6646d0c47cc56d5c108b6d99b790b9d7692
ABHISHEK <abhidk62@gmail.com>
2025-11-22 23:10:02 +0530

    Time: 2 ms (29.52%), Space: 56.4 MB (91.58%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+let n=nums.length
+    for(let i=0;i<n;i++)
+    {
+        nums[i+n]=nums[i]
+    }
+    return nums
+};
