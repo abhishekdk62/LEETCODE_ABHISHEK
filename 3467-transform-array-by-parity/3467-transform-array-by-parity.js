f105a72e4e57a647d8c2bb6482d122cc79fa5954
ABHISHEK <abhidk62@gmail.com>
2025-11-25 09:11:36 +0530

    Time: 3 ms (38.6%), Space: 57.1 MB (67.78%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.js
@@ -0,0 +1,10 @@
+var transformArray = function(nums) {
+ 
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] % 2 === 0 ? 0 : 1;
+    }
+
+    nums.sort((a, b) => a - b);
+
+    return nums;
+};
