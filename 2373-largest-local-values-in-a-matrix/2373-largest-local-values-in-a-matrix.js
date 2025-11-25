964b4c22dc5eccec92173fcf0b1674a29e74b70f
ABHISHEK <abhidk62@gmail.com>
2025-11-25 16:55:30 +0530

    Time: 4 ms (65.48%), Space: 58.6 MB (59.52%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.js
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.js
@@ -3,26 +3,23 @@
  * @return {number[][]}
  */
 var largestLocal = function (grid) {
-let res=[]
-    for (let i = 1; i < grid.length-1; i++) {
-        let row=[]
-        for (let j = 1; j < grid[0].length-1; j++) {
+    let res = []
+    for (let i = 1; i < grid.length - 1; i++) {
+        let row = []
+        for (let j = 1; j < grid[0].length - 1; j++) {
+            let maxval = grid[i][j]
+            for (let r = i - 1; r < i+2; r++) {
+                for (let p = j - 1; p < j+2; p++) {
+                    if (grid[r][p] > maxval) {
+                        maxval = grid[r][p]
+                    }
+                }
+            }
 
-            let v1=grid[i][j]
-            let v2=grid[i-1][j]
-            let v3=grid[i][j-1]
-            let v4=grid[i-1][j-1]
-            let v5=grid[i][j+1]  
-            let v6=grid[i+1][j]  
-            let v7=grid[i-1][j+1]  
-            let v8=grid[i+1][j-1]  
-            let v9=grid[i+1][j+1]  
-
-            let value=Math.max(v1,v2,v3,v4,v5,v6,v7,v8,v9)
-        row.push(value)
+            row.push(maxval)
 
         }
         res.push(row)
     }
-return res
+    return res
 };
