665bee6f7a986acbf956deecb763cdec1c51d3e9
ABHISHEK <abhidk62@gmail.com>
2025-11-26 12:16:44 +0530

    Time: 0 ms (100%), Space: 58.7 MB (6.19%) - LeetHub

---
+++ 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.js
@@ -0,0 +1,9 @@
+var stableMountains = function(height, threshold) {
+    let res = []
+    for (let i = 1; i < height.length; i++) {
+        if (height[i - 1] > threshold) {
+            res.push(i)
+        }
+    }
+    return res
+};
