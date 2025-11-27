7ec474e60406ec2340cb05da7e2c3007ebcc3f15
ABHISHEK <abhidk62@gmail.com>
2025-11-27 11:18:59 +0530

    Time: 1 ms (8.96%), Space: 55.5 MB (55.97%) - LeetHub

--- 3516-find-closest-person/3516-find-closest-person.js
+++ 3516-find-closest-person/3516-find-closest-person.js
@@ -1,3 +1,24 @@
+// /**
+//  * @param {number} x
+//  * @param {number} y
+//  * @param {number} z
+//  * @return {number}
+//  */
+// var findClosest = function(x, y, z) {
+//     let dif1=Math.abs(z-x)
+//     let dif2=Math.abs(z-y)
+//     if(dif1>dif2)
+//     {
+//         return 2
+//     }else if(dif1<dif2)
+//     {
+//         return 1
+//     }else
+//     {
+//         return 0
+//     }
+// };
+
 /**
  * @param {number} x
  * @param {number} y
@@ -5,16 +26,8 @@
  * @return {number}
  */
 var findClosest = function(x, y, z) {
-    let dif1=Math.abs(z-x)
-    let dif2=Math.abs(z-y)
-    if(dif1>dif2)
-    {
-        return 2
-    }else if(dif1<dif2)
-    {
-        return 1
-    }else
-    {
-        return 0
-    }
+    let dif1 = Math.abs(z - x)
+    let dif2 = Math.abs(z - y)
+
+    return dif1 === dif2 ? 0 : (dif1 < dif2 ? 1 : 2)
 };
