b7f11977c580daa25897b96c1f2c953cc8f33eb1
ABHISHEK <abhidk62@gmail.com>
2025-11-28 10:30:30 +0530

    Time: 0 ms (100%), Space: 54.1 MB (46.83%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -2,14 +2,32 @@
  * @param {number} num
  * @return {boolean}
  */
+// var isPerfectSquare = function(num) {
+
+//     let val=Math.sqrt(num)
+//     console.log(val)
+
+//     if(val.toString().includes('.'))
+//     {
+//         return false
+//     }
+//     return true
+// };
+
+
 var isPerfectSquare = function(num) {
-
-    let val=Math.sqrt(num)
-    console.log(val)
-
-    if(val.toString().includes('.'))
-    {
-        return false
+    if (num < 2) return true; 
+    let left = 2, right = num / 2;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let squared = mid * mid;
+        if (squared === num) {
+            return true; 
+        } else if (squared < num) {
+            left = mid + 1; 
+        } else {
+            right = mid - 1; 
+        }
     }
-    return true
+    return false; 
 };
