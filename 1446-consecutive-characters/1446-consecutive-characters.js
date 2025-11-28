6b3035362b1083c37e0a70574d27fbd44495dbb3
ABHISHEK <abhidk62@gmail.com>
2025-11-28 09:02:21 +0530

    Time: 3 ms (20.53%), Space: 56.9 MB (13.16%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -3,24 +3,17 @@
  * @return {number}
  */
 var maxPower = function (s) {
-    let seen = new Set()
     let max = 0
 
     for (let i = 0; i < s.length; i++) {
-
-    let count = 1
-        let char = s[i]
-
-        while (char == s[i + 1]) {
-         
+        let count = 1
+        while (s[i] == s[i + 1]) {
             count++
             i = i + 1
-
         }
         if (count > max) {
             max = count
         }
-
     }
-return max
+    return max
 };
