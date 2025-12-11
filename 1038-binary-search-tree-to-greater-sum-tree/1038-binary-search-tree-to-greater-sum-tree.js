40686a13bb3b75e4dea2a7bbfd3c3916cfcfb470
ABHISHEK <abhidk62@gmail.com>
2025-12-11 15:49:51 +0530

    Time: 0 ms (100%), Space: 55.2 MB (70.21%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,14 @@
+var bstToGst = function(root) {
+    let sum = 0;
+    
+    const dfs = (node) => {
+        if (!node) return;
+        dfs(node.right);
+        sum += node.val;
+        node.val = sum;
+        dfs(node.left);
+    };
+    
+    dfs(root);
+    return root;
+};
