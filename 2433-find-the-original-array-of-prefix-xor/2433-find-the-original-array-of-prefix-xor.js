3df8eb8242d52584d3e7bbffc29a3f94632f27a3
ABHISHEK <abhidk62@gmail.com>
2025-12-11 15:50:16 +0530

    Time: 3 ms (62.12%), Space: 87.1 MB (46.97%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -0,0 +1,9 @@
+var findArray = function(pref) {
+    const n = pref.length;
+    const arr = Array(n);
+    arr[0] = pref[0];
+    for (let i = 1; i < n; i++) {
+        arr[i] = pref[i] ^ pref[i - 1];
+    }
+    return arr;
+};
